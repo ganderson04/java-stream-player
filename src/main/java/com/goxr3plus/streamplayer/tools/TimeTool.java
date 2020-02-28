ed8f1f0aed377f72ce841f9ceeab3510f2baaee3
George Anderson <gjanderson04@gmail.com>
2020-02-28 22:04:54 +0000

    Changed OGG duration calculation.

--- src/main/java/com/goxr3plus/streamplayer/tools/TimeTool.java
+++ src/main/java/com/goxr3plus/streamplayer/tools/TimeTool.java
@@ -1,7 +1,10 @@
 package com.goxr3plus.streamplayer.tools;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
@@ -142,8 +145,16 @@ public final class TimeTool {
 				}
 				// }
 			}
-			// WAVE || OGG?
-			else if ("ogg".equals(extension) || "wav".equals(extension)) {
+			// OGG?
+			else if("ogg".equals(extension)) {
+				try {
+					milliseconds = calculateOggVorbisAudioDuration(file);
+				} catch (IOException ex) {
+					System.err.println("Problem getting the time of-> " + file.getAbsolutePath());
+				}
+			}
+			// WAV
+			else if ("wav".equals(extension)) {
 				try (AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file)) {
 					final AudioFormat format = audioInputStream.getFormat();
 					milliseconds = (int) (file.length() / (format.getFrameSize() * (int) format.getFrameRate())) * 1000;
@@ -155,6 +166,54 @@ public final class TimeTool {
 
 		// System.out.println("Passed with error")
 		return milliseconds < 0 ? -1 : milliseconds;
+	}
+
+	// Adapted from https://stackoverflow.com/a/44407355/8100469
+	private static long calculateOggVorbisAudioDuration(File oggFile) throws IOException {
+		int rate = -1;
+		int length = -1;
+
+		int size = (int) oggFile.length();
+		byte[] t = new byte[size];
+
+		try(FileInputStream stream = new FileInputStream(oggFile)) {
+			stream.read(t);
+
+			for (int i = size - 1 - 8 - 2 - 4; i >= 0 && length < 0; i--) { //4 bytes for "OggS", 2 unused bytes, 8 bytes for length
+				// Looking for length (value after last "OggS")
+				if (
+						t[i] == (byte) 'O'
+								&& t[i + 1] == (byte) 'g'
+								&& t[i + 2] == (byte) 'g'
+								&& t[i + 3] == (byte) 'S'
+				) {
+					byte[] byteArray = new byte[]{t[i + 6], t[i + 7], t[i + 8], t[i + 9], t[i + 10], t[i + 11], t[i + 12], t[i + 13]};
+					ByteBuffer bb = ByteBuffer.wrap(byteArray);
+					bb.order(ByteOrder.LITTLE_ENDIAN);
+					length = bb.getInt(0);
+				}
+			}
+			for (int i = 0; i < size - 8 - 2 - 4 && rate < 0; i++) {
+				// Looking for rate (first value after "vorbis")
+				if (
+						t[i] == (byte) 'v'
+								&& t[i + 1] == (byte) 'o'
+								&& t[i + 2] == (byte) 'r'
+								&& t[i + 3] == (byte) 'b'
+								&& t[i + 4] == (byte) 'i'
+								&& t[i + 5] == (byte) 's'
+				) {
+					byte[] byteArray = new byte[]{t[i + 11], t[i + 12], t[i + 13], t[i + 14]};
+					ByteBuffer bb = ByteBuffer.wrap(byteArray);
+					bb.order(ByteOrder.LITTLE_ENDIAN);
+					rate = bb.getInt(0);
+				}
+
+			}
+		}
+
+		int duration = length / rate;
+		return (duration*1000);
 	}
 
 }
